334f2a683f1a990c04fd8068e0aced747fbeb034
kiyoxi2020 <76893054+kiyoxi2020@users.noreply.github.com>
2021-11-28 20:08:33 +0800

    Add files via upload

---
+++ code2/build/CMakeFiles/main.dir/src/main.cpp.obj.d
@@ -0,0 +1,20 @@
+CMakeFiles/main.dir/src/main.cpp.obj: E:\opengl\code2\src\main.cpp \
+ E:/opengl/code2/include/glad/glad.h \
+ E:/opengl/code2/include/KHR/khrplatform.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/11.2.0/include/stdint.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/stdint.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/corecrt.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/11.2.0/include/stddef.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/stddef.h \
+ E:/opengl/code2/include/GLFW/glfw3.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/stdio.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/corecrt_stdio_config.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/_mingw_off_t.h \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/swprintf.inl \
+ C:/msys64/mingw64/x86_64-w64-mingw32/include/sec_api/stdio_s.h
